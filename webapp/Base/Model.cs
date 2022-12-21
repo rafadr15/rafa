36ae3ab7c84addfed4f9c9dfcb408f9a62dd37e7
Rafaela <rafdrag47@gmail.com>
2022-12-21 15:48:41 +0200

    delete

---
+++ webapp/Base/Model.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace webapp.Base
+{
+    public class Model
+    {
+        public string id { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
