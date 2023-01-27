a5c473437cd8a135e961c2c5b43980270a59442a
Rafaela <rafdrag47@gmail.com>
2023-01-27 17:38:48 +0200

    Edit AppDbContext

---
+++ webapp/Database/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using webapp.Features.Assignments.Models;
+
+namespace webapp.Database;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions options):base(options){}
+
+    public DbSet<AssignmentModel> Assignments { get; set; }
+ 
+    
+}
--- webapp/Program.cs
+++ webapp/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using webapp.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
